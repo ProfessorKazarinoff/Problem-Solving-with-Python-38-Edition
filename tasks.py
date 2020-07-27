59e1fe7fe715523878ea435d662b7722d8242773
Peter Kazarinoff <peter.kazarinoff@pcc.edu>
2020-07-27 15:28:31 -0700

    add tasks.py and templates dir with mkdocs.yml and preamble.py

---
+++ tasks.py
@@ -0,0 +1,40 @@
+# tasks.py
+"""
+An invoke script to run build tasks for the book
+
+invoke build # this builds the mkdocs website website
+
+then cd into website directory and run mkdocs serve to view
+"""
+
+from invoke import task
+from pathlib import Path
+from shutil import copyfile, rmtree, copytree
+from os import chdir, getcwd
+
+@task
+def build(c):
+    """
+    a task to build an mkdocs website from the src notebooks
+    """
+    print("Building...")
+    if Path('website'):
+        print("Found website directory, deleting...")
+        rmtree('website')
+    print("Creating empty website directory...")
+    Path('website').mkdir(parents=True, exist_ok=True)
+    mkdocs_config_template = Path('templates','mkdocs.yml')
+    mkdocs_build_config = Path('website','mkdocs.yml')
+    preamble_template = Path('templates','preamble.py')
+    preamble_file = Path('website','preamble.py')
+    print("Copying mkdocs.yml and preamble.py from templates/ to website/")
+    copyfile(mkdocs_config_template,mkdocs_build_config)
+    copyfile(preamble_template,preamble_file)
+    print("Copying src/ into website/docs/")
+    copytree(Path('src'),Path('website','docs'))
+    print("Changing to website directory")
+    chdir('website')
+    print(f'Current working directory {getcwd()}')
+    print("running mkdocs build...")
+    print()
+    c.run("mkdocs build")
---
+++ templates/preamble.py
@@ -0,0 +1,7 @@
+# templates/preabmle.py
+"""
+a file that runs before the mkdocs build step
+when using the mknotebooks extension
+"""
+
+print("Running preambly.py before mkdocs build")
